0b97fcec5cc6a8207378254bc7c0a3e44e6839fa
Brian McGarry <bmcgarry816@gmail.com>
2019-11-25 15:02:25 -0800

    remove extra column, home_rain_game, from calculate_aggregate_stats_pandas

--- darksky_soccer.py
+++ darksky_soccer.py
@@ -25,11 +25,11 @@ def get_season_data(database, season):
         season int: year of interest for match data
     Returns:
         pandas dataframe with match stats'''
-    
+
     conn = sqlite3.connect(database)
     c = conn.cursor()
-    c.execute(f"""SELECT * 
-                 FROM Matches 
+    c.execute(f"""SELECT *
+                 FROM Matches
                  WHERE Season = {season}
                  AND Div in ('D1', 'E0')
                  ;""")
@@ -47,19 +47,19 @@ def get_weather_data(date, password, lat='52.5200', long='13.4050'):
     Returns: tuple of date, rain
         date str: date the weather checked in YYYY-MM-DD format
         rain bool: Did it rain this day'''
-    
+
     time = date + 'T00:00:00'
     result = requests.get(f'https://api.darksky.net/forecast/{password}/{lat},{long}, {time}').json()
 
     icon = result['daily']['data'][0].get('icon')
     rain = icon == 'rain'
-    
+
     return date, rain
 
 def rain_dates(unique_dates, password):
     '''
     Creates a dataframe consisting of dates and Boolean values indicating if it rained that day
-    
+
     Args:
         unique_dates list[str]: list of dates in YYYY-MM-DD format
         password str: api key for accessing the Darksky api
@@ -71,14 +71,14 @@ def rain_dates(unique_dates, password):
         date, rain = get_weather_data(date, password)
         data.append({'Date': date,
                      'rain': rain,})
-        
+
     return pd.DataFrame(data)
 
 def match_rain_data(database, season, password, limit_api_calls=-1):
     '''
     Combines rain data from the darksky api with match data from a local sqlite database
     '''
-    
+
     match_data = get_season_data('database.sqlite', season)
 
     unique_match_dates = match_data['Date'].unique()[:limit_api_calls]
@@ -90,7 +90,6 @@ def calculate_aggregate_stats_pandas(df):
     #Calculate Home stats
     home_df = df.copy()
     home_df['home_win'] = home_df['FTHG'] > home_df['FTAG']
-    home_df['rain_home_game'] = home_df['FTR'] == 'H'
     home_df['rain_home_win'] = (home_df['home_win']) & (home_df['rain'])
     home_stats = home_df.groupby(['HomeTeam']).sum()[['FTHG', 'home_win', 'rain_home_win', 'rain']]
 
@@ -121,27 +120,27 @@ def calculate_aggregate_stats_pandas(df):
     final_df.rename(columns={'HomeTeam':'team_name',
                              'Div': 'division',
                              'Season': 'season'}, inplace=True)
-    
+
     return final_df.to_dict(orient='records')
 
 def calculate_aggregate_stats_sqlite(match_stats_df, database):
-    
+
     conn = sqlite3.connect(database)
     match_stats_df.to_sql('temp_rain_stats', con=conn, if_exists='replace')
     c = conn.cursor()
-    
+
     agg_query = '''
-    SELECT 
+    SELECT
       HomeTeam as Club,
       Div,
       Season,
       (h_win + a_win) as W,
       (h_goals_for+a_goals_for) as GF,
       round((a_rain_win + h_rain_win) * 1.0 / (h_rain_games + a_rain_games), 2) as rain_win_pct
-      
+
       FROM
-                
-      (SELECT Div, HomeTeam, 
+
+      (SELECT Div, HomeTeam,
              SUM(CASE WHEN FTHG > FTAG THEN 1 ELSE 0 END) as h_win,
              SUM(CASE WHEN FTHG = FTAG THEN 1 ELSE 0 END) as h_draw,
              SUM(CASE WHEN FTHG < FTAG THEN 1 ELSE 0 END) as h_loss,
@@ -153,36 +152,36 @@ def calculate_aggregate_stats_sqlite(match_stats_df, database):
        FROM temp_rain_stats
        GROUP BY HomeTeam
        ORDER BY HomeTeam)
-            
+
       JOIN
-            
-      (SELECT AwayTeam, 
+
+      (SELECT AwayTeam,
              SUM(CASE WHEN FTAG > FTHG THEN 1 ELSE 0 END) as a_win,
              SUM(CASE WHEN FTAG > FTHG AND rain == 1 THEN 1 ELSE 0 END) as a_rain_win,
              SUM(rain) as a_rain_games,
              SUM(FTAG) as a_goals_for
-              
+
        FROM temp_rain_stats
        GROUP BY AwayTeam
        ORDER BY AwayTeam)
-            
+
        ON (HomeTeam==AwayTeam)
-            
+
        ORDER BY GF DESC'''
-    
+
     c.execute(agg_query)
-    
+
     df = pd.DataFrame(c.fetchall(),
                       columns=[x[0] for x in c.description])
-    
+
     c.execute('''DROP TABLE temp_rain_stats''')
-    
+
     return df.to_dict(orient='records')
 
 def insert_to_atlas(atlas_user, atlas_key, cluster_name, collection_name, team_stats, return_ids=False):
     '''
     Inserts multiple documents into a specified mongodb atlas instance
-    
+
     Args:
         atlas_user str: username
         altas_key str: api key for cluster
@@ -195,9 +194,9 @@ def insert_to_atlas(atlas_user, atlas_key, cluster_name, collection_name, team_s
     client = pymongo.MongoClient(f'mongodb+srv://{atlas_user}:{atlas_key}@{cluster_name}.mongodb.net/test?retryWrites=true&w=majority')
     db = client.test
     collection = db[collection_name]
-    
+
     result = collection.insert_many(team_stats,
                                     ordered=False)
-    
+
     if return_ids:
         return result.inserted_ids
